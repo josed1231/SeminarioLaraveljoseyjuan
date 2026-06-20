0be902a3f8cd36be511cfc8cb60d0e63776c709b
josed1231 <josedavidzuluagabarco@gmail.com>
2026-06-20 13:14:47 -0500

    Agregar atributo categoria, getter y modificar getInfo()

---
+++ src/POO/producto.php
@@ -0,0 +1,31 @@
+<?php
+class Producto {
+    private $nombre;
+    private $precio;
+    private $iva;
+    private $categoria;
+
+    public function __construct($nombre, $precio, $iva = 0.19, $categoria = "General") {
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->iva = $iva;
+        $this->categoria = $categoria;
+    }
+
+    public function getPrecioFinal() {
+        return $this->precio * (1 + $this->iva);
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getCategoria() {
+        return $this->categoria;
+    }
+
+    public function getInfo() {
+        return "Producto: {$this->nombre} | Categoría: {$this->categoria} | Precio final: $" . $this->getPrecioFinal();
+    }
+}
+?>
--- src/index.php
+++ src/index.php
@@ -1,3 +1,16 @@
 <?php
-	phpinfo();
+require_once 'POO/Producto.php';
+
+$prod1 = new Producto("laptop gamer", 1500);
+$prod2 = new Producto("mouse inalambrico", 80);
+$prod3 = new Producto("monitor 4K", 1200, 0.19);
+
+echo "<h2>Catalogo POO</h2>";
+echo "<ul>";
+echo "<li>" . $prod1->getInfo() . "</li>";
+echo "<li>" . $prod2->getInfo() . "</li>";
+echo "<li>" . $prod3->getInfo() . "</li>";
+echo "</lu>";
+
+
 ?>
